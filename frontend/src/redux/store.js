6bc1db381e175d4e351208c423cb9292f4f22e82
Laxmidkl <dhakalaxmi74@gmail.com>
2025-07-02 21:08:16 +0545

    Add Redux Toolkit setup in frontend (cartSlice, store, Provider)

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import cartReducer from "../redux/features/cart/cartSlice"
+
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    cart: cartReducer
+  },
 })
