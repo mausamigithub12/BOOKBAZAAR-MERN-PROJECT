6ef59f529f32eaaa3022d7eefd1a7a7442c04b07
Laxmidkl <dhakalaxmi74@gmail.com>
2025-07-03 22:04:19 +0545

    Create CheckoutPage with form validation and order handling

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -8,6 +8,7 @@ import Home from "../pages/home/Home";
 import Login from "../components/Login";
 import Register from "../components/Register";
 import CartPage from "../pages/books/CartPage";
+import CheckoutPage from "../pages/books/CheckoutPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -16,10 +17,11 @@ const router = createBrowserRouter(
       <Route path="/dashboard" element={<h1>Dashboard</h1>} />
       <Route path="/orders" element={<h1>Orders</h1>} />
       {/* <Route path="/cart" element={<h1>Cart Page</h1>} /> */}
-      <Route path="/checkout" element={<h1>Check Out</h1>} />
+      {/* <Route path="/checkout" element={<h1>Check Out</h1>} /> */}
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register/>}/>
       <Route path="/cart" element={<CartPage/>}/>
+      <Route path="/checkout" element={ <CheckoutPage/> }/>
     </Route>
   )
 );
