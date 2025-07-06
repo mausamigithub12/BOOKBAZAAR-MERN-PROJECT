01c59c7060e6e0433402249ff83de4790e130833
Laxmidkl <dhakalaxmi74@gmail.com>
2025-07-06 22:43:21 +0545

    Implement CRUD operations for Book resource in book.route.js and book.controller.js

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,30 @@
 const express = require("express");
 const app = express();
-
+const cors = require("cors");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
-require('dotenv').config()
+//middleware
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173/"],
+    credentials: true,
+  })
+);
 
+//route
+const bookRoutes = require("./src/books/book.route.js");
+app.use("/api/books", bookRoutes);
+
+app.get("/", (req, res) => {
+  res.send(" Book Bazar server is live and running fine");
+});
 
 async function main() {
-  await mongoose.connect(
-   process.env.MONGO_URI
-  );
-  app.get("/", (req, res) => {
-    res.send(" Book Bazar server is live and running fine");
-  });
+  await mongoose.connect(process.env.MONGO_URI);
 }
 
 main()
@@ -24,5 +34,3 @@ main()
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-//fqZYpfLNq0FoiYll
---
+++ backend/src/books/book.route.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router =  express.Router();
+const Book = require('./book.model');
+const { postABook, getAllBooks, getSingleBook, updatedBook, deleteABook} = require('./book.controller');
+
+
+
+router.post("/create-book", postABook)
+
+//get all books 
+router.get("/", getAllBooks)
+
+// single book endpoint
+router.get("/:id", getSingleBook);
+
+// update a book endpoint
+router.put("/edit/:id",  updatedBook);
+  
+router.delete("/:id", deleteABook)
+
+
+module.exports = router;
+
+//post = when submit something fronted to db
+// get =  when get something back from db
+// put/patch = when edit or update something
+// delete = when delete something
